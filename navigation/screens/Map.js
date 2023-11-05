190f282d705aad24e263fe7f913f9d57c13e21f6
bksteedly <bs102@wellesley.edu>
2023-11-04 23:12:33 -0400

    maps

--- navigation/screens/Map.js
+++ navigation/screens/Map.js
@@ -1,13 +1,43 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function Map({navigation}){
-    return(
-        <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Text 
-                onPress = {()=>navigation.navigate('Home')}
-                style={{fontSize:26, fontWeight:'bold'}}
-            >Map Screen</Text>
-        </View>
-    );
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+      <Text 
+              onPress = {()=>navigation.navigate('Home')}
+              style={{fontSize:26, fontWeight:'bold'}}
+          >Map Screen</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff', 
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+
+
+// import * as React from 'react';
+// import {View, Text} from 'react-native';
+
+// export default function Map({navigation}){
+//   return(
+//       <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
+//           <Text 
+//               onPress = {()=>navigation.navigate('Home')}
+//               style={{fontSize:26, fontWeight:'bold'}}
+//           >Map Screen</Text>
+//       </View>
+//   );
+// }
